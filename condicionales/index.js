9069f4fc8fe1cd20ddb12045ebc99941cb6d9b4e
angelpalomino08 <angelpalominolliulla@iejoseobrero.edu.pe>
2023-05-08 08:02:38 -0500

    0.0.2

---
+++ condicionales/index.js
@@ -0,0 +1,27 @@
+// ejercicio 1
+//if ("0") {
+//    alert ( "hello")
+//}
+//ejercicio 2
+
+/*let respuesta = prompt("cual es el nombre oficial de JavaScript");
+
+if (respuesta == ECMAScript) {
+    alert ("correcto")
+} else {
+    alert ( "no lo sabes?  ¡ECMAScript!")
+}*/
+
+//ejercicio 3 
+
+let numero = prompt("inserta un número");
+
+if ( numero == "0" ) {
+    alert("0")
+} else if (numero < 0) {
+    alert("-1")
+} else  if(numero > 0 ) {
+    alert("1")
+} else {
+    alert("no se registro número")
+}
